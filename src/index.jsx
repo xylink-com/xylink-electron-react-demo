fc775f26a077dde4b568c65cdcef0d003b95e3de
luojinghui <luojinghui@xylink.com>
2020-08-19 14:15:11 +0800

    feat: init project

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./style/index.scss";
-
-ReactDOM.render(<App />, document.getElementById("app"));
