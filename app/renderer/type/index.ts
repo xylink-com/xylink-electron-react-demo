4506ac00d478ca6260760a310da560ee636ed14d
jinghui <luojinghui@xylink.com>
2021-02-02 17:58:23 +0800

    feat: add type

--- app/renderer/type/index.ts
+++ app/renderer/type/index.ts
@@ -14,4 +14,41 @@ export interface IConfControl {
   chirmanUri: string;
   // 会控web url地址，打开即可进行会议控制
   confMgmtUrl: string;
+  // 是否强制静音
+  disableMute:boolean;  
+  // 静音、非静音
+  muteMic:string;
+}
+
+export interface IConfInfoChanged {
+  participantCount:number;
+  contentPartCount:number;
+  chairManUrl:string;
+}
+
+export interface ISelectedDevice {
+  isSelected:boolean;
+  devId:string;
+}
+
+export type TDeviceType = "camera" | "microphone" | "speaker" | string;
+
+export type TModel = 'custom'|'auto';
+
+export type TResolutionType = 0 | 1 | 2 | 3 | 4 | 5;
+
+export interface ITemplateObj {
+  position:number[]
+}
+
+export interface ITemplate  {
+  [type:string]:{
+    length:number;
+    temp:{
+      [index:string]:ITemplateObj[];
+    };
+    rate:{
+      [index:number]:number
+    }
+  }
 }
